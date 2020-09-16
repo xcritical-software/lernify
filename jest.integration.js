190c19866942bfe95a1bcdfa46ac947eb026b0c1
Sergey Bondar <sergeyb@xcritical.software>
2020-09-16 15:48:38 +0300

    add tests

---
+++ jest.integration.js
@@ -0,0 +1,20 @@
+// "use strict";
+
+// module.exports = {
+//   modulePathIgnorePatterns: ["/__fixtures__/"],
+//   roots: ["<rootDir>/integration"],
+//   setupFiles: ["@lerna-test/set-npm-userconfig"],
+//   setupFilesAfterEnv: ["<rootDir>/setup-integration-timeout.js"],
+//   snapshotSerializers: ["@lerna-test/serialize-placeholders"],
+//   testEnvironment: "node",
+//   testRunner: "jest-circus/runner",
+//   verbose: true,
+// };
+
+// // split tests into smaller chunks because windows is agonizingly slow
+// if (process.env.CI && process.env.TRAVIS_OS_NAME === "windows") {
+//   module.exports.testMatch =
+//     process.env.LERNA_CI_TYPE === "publish"
+//       ? ["<rootDir>/integration/@(lerna-publish)*.test.js"]
+//       : ["<rootDir>/integration/!(lerna-publish)*.test.js"];
+// }
--- packages/package3/webpack.config.js
+++
@@ -1,8 +0,0 @@
-const { createConfig } = require('../../webpack.config')
-const path = require("path");
-
-module.exports = createConfig({
-  port: 3002,
-  template: path.resolve(__dirname, "index.html"),
-  projectPath: __dirname
-})
